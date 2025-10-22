ef63705fbdf80d47e40b71fcb5cc80a72f23c456
S-kalyanasundaram <148667500+S-kalyanasundaram@users.noreply.github.com>
2025-10-22 12:51:24 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://38cc92e0-f706-4259-992f-2f26420dbae0-00-26jzlejsgu0zm.sisko.replit.dev/"//"https://api.fifpclub.com/fifpchatbot/"//"http://127.0.0.1:8000",//"https://fifpbackendser-production.up.railway.app/"
+  baseURL: "https://api.fifpclub.com/fifpchatbot/"//"http://127.0.0.1:8000"//"https://38cc92e0-f706-4259-992f-2f26420dbae0-00-26jzlejsgu0zm.sisko.replit.dev/"//"https://api.fifpclub.com/fifpchatbot/"//"http://127.0.0.1:8000",//"https://fifpbackendser-production.up.railway.app/"
 });
 
 export const loadData = (userId) => API.post(`/load-data/${userId}`);
@@ -15,4 +15,5 @@ export const deleteSession = (userId, sessionId) =>
 
 export const renameSession = (userId, sessionId, title) =>
   API.put(`/chat-sessions/${userId}/${sessionId}`, { title });
+
 export const getUserName = (userId) => API.get(`/user-name/${userId}`);
